2722d9ef756450cc297bb28a0642ce7a2c0e4539
Kirill <b1nc0d3.kka@gmail.com>
2024-04-06 19:33:47 +0300

    fix: moved some sensitive data to .env

--- config.py
+++ config.py
@@ -4,20 +4,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+ADMINS_IDS = os.getenv('ADMINS_ID')
 TG_TOKEN = os.getenv('TG_TOKEN')
 FOLDER_ID = os.getenv('FOLDER_ID')
 GPT_TOKEN = os.getenv('GPT_TOKEN')
 
-TOKENIZER_URL = 'https://llm.api.cloud.yandex.net/foundationModels/v1/tokenize'
-GPT_URL = 'https://llm.api.cloud.yandex.net/foundationModels/v1/completion'
+TOKENIZER_URL = os.getenv('TOKENIZER_URL')
+GPT_URL = os.getenv('GPT_URL')
 
-GPT_MODEL = 'yandexgpt-lite'
+GPT_MODEL = os.getenv('GPT_MODEL')
+
 MAX_TOKENS_IN_SESSION = 500
 MAX_TOKENS_FOR_ANSWER = 200
 MAX_SESSIONS = 3
 MAX_USERS = 3
-
-ADMINS_IDS = [236725960]
 
 HEADERS = {'Authorization': f'Bearer {GPT_TOKEN}'}
 DEFAULT_DATA = {
